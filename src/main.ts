7846b8d1467b7eb21ffba03a89de37daddebf66a
RShnitser <romanshnitser@hotmail.com>
2025-03-28 23:07:53 -0400

    add player hand

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
-
-import {Suit, Rank} from "./game_constants"
+import { Suit, Rank } from "./game_constants";
 
 type Card = {
-  suit: Suit,
-  rank: Rank,
-}
-
+  suit: Suit;
+  rank: Rank;
+};
 
 function createDeck() {
-  const result : Card[] = [];
-  
+  const result: Card[] = [];
+
   for (const suit of Object.values(Suit)) {
     for (const rank of Object.values(Rank)) {
-      const card : Card = {suit, rank};
+      const card: Card = { suit, rank };
       result.push(card);
     }
   }
@@ -29,17 +27,21 @@ function createDeck() {
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 let prevTime = 0;
+const state: GameState = {
+  deck: [],
+  playerOneHand: [],
+};
 
 type Button = {
-  isDown: boolean,
-  isChanged: boolean,
-}
+  isDown: boolean;
+  isChanged: boolean;
+};
 
 type Input = {
-  mouseX: number,
-  mouseY: number,
-  action: Button,
-}
+  mouseX: number;
+  mouseY: number;
+  action: Button;
+};
 
 const input: Input = {
   mouseX: 0,
@@ -48,6 +50,11 @@ const input: Input = {
     isDown: false,
     isChanged: false,
   },
+};
+
+type GameState = {
+  deck: Card[];
+  playerOneHand: Card[];
 };
 
 function processInputState(button: Button, isDown: boolean) {
@@ -98,11 +105,23 @@ function isButtonReleased(button: Button) {
   return result;
 }
 
+function drawCards(hand: Card[], deck: Card[], amount: number) {
+  amount = Math.min(amount, deck.length);
+  for (let i = 0; i < amount; i++) {
+    const card = deck.pop();
+    if (card !== undefined) {
+      hand.push(card);
+    }
+  }
+}
+
 function main() {
   window.addEventListener("mousedown", handleMouseDown);
   window.addEventListener("mouseup", handleMouseUp);
   window.addEventListener("mousemove", handleMouseMove);
-  const deck = createDeck();
+
+  state.deck = createDeck();
+  drawCards(state.playerOneHand, state.deck, 6);
 
   window.requestAnimationFrame(update);
 }
@@ -112,12 +131,19 @@ function update() {
   const deltaTime = (now - prevTime) * 0.001;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "grey";
+  ctx.fillRect(0, 0, 800, 600);
   ctx.fillStyle = "blue";
 
   if (isButtonDown(input.action)) {
     ctx.fillStyle = "yellow";
   }
   ctx.fillRect(input.mouseX, input.mouseY, 10, 10);
+
+  ctx.fillStyle = "white";
+  for (let i = 0; i < state.playerOneHand.length; i++) {
+    ctx.fillRect(i * 100, 50, 50, 100);
+  }
   //ctx.ellipse(input.mouseX, input.mouseY, 5, 5, 0, 0, 2 * Math.PI);
   prevTime = now;
   window.requestAnimationFrame(update);
